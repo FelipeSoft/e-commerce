772ff7fd497b654ecff39ac88dda85f2180c1883
FelipeSoft <felipedecastrolima2@gmail.com>
2023-08-27 13:01:40 -0300

    fix: Adicionando as quantidade corretamente e atualizando o preço do carrinho

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,6 @@
-import { MdOutlineCancel } from "react-icons/md";
+import { BsArrowLeft } from "react-icons/bs";
 import { useAppSelector } from "../redux/hooks/useAppSelector";
+import { productFeatures } from "../utils/productFeatures";
 
 type Props = {
     children: React.ReactNode;
@@ -11,10 +12,18 @@ export const Cart = ({ onClick, disable, children }: Props) => {
     const cart = useAppSelector(state => state.cart);
 
     return (
-        <div className={`h-screen bg-white w-1/3 fixed top-0 bottom-0 right-0 z-30 overflow-y-auto transition-all ${disable && "-mr-cart"} ${!disable && "mr-0"}`}>
-            <div className="w-full bg-indigo-700 h-14 px-8 text-white font-medium text-lg flex items-center cursor-pointer" onClick={onClick}>
-                <MdOutlineCancel className="text-3xl text-white cursor-pointer mr-2"/>
-                Fechar
+        <div className={`h-screen shadow-xl shadow-black bg-white w-1/3 fixed right-0 bottom-0 top-0 z-40 overflow-y-auto transition-all ${disable && "-mr-cart"} ${!disable && "mr-0 mr-"}`}>
+            <div className="w-full bg-indigo-700 h-14 px-8 text-white font-medium text-lg flex items-center justify-between cursor-pointer" onClick={onClick}>
+                <div className="flex items-center">
+                    <BsArrowLeft className="text-3xl text-white cursor-pointer mr-4 "/>
+                    <p>Meu Carrinho</p>
+                </div>
+                <span>R$ {productFeatures.calculateTotalCartPrice(cart.products).toLocaleString("pt-BR", {
+                    style: "decimal",
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2
+                    })}
+                </span>
             </div>
             <div className={`w-full flex flex-col ${ cart.products.length === 0 && "items-center justify-center"} h-cart`}>
                 { children }
