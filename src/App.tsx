6e0f78ad11fe77b8838696a14c8f6415cbdaacc7
FelipeSoft <felipedecastrolima@gmail.com>
2023-09-20 10:35:27 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,6 @@ const App = () => {
                         </CartProduct>
                     );
                 })}
-                
             </Cart>
             <MasterRoutes />
         </div>
